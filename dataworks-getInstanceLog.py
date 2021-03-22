78c7ecd2d7d4bcf469922f5e405f7cef89195cfc
jeason <779022431@qq.com>
2021-03-22 20:23:14 +0800

    1

--- dataworks-getInstanceLog.py
+++ dataworks-getInstanceLog.py
@@ -3,15 +3,20 @@ import json
 import sys
 from util import App
 from util import unicode_convert
+import util
 
 reload(sys)
 sys.setdefaultencoding('utf8')
-
 app = App().getDataworksInstance()
-ret = app.doAction('GetInstanceLog', {'InstanceId': 9716662351, 'ProjectEnv': 'PROD'})
-if ret['code'] == 0:
-    data = json.loads(ret['data'], 'utf-8')
-    data = unicode_convert(data)
-    print 'InstanceId: {},ErrorCode: {}, ErrorMessage: {}'.format(data['InstanceId'], data['Status'], data['ErrorMessage'])
-else:
-    print(ret['message'])
+dirPath = app.config.env('app', 'dirpath')
+str2 = util.readFile(dirPath + '/instances.txt')
+str2 = str2.strip('\n')
+ids = util.explode(',', str2)
+for id in ids:
+    ret = app.doAction('GetInstanceLog', {'InstanceId': id, 'ProjectEnv': 'PROD'})
+    if ret['code'] == 0:
+        data = json.loads(ret['data'], 'utf-8')
+        data = unicode_convert(data)
+        print 'InstanceId: {},ErrorCode: {}, ErrorMessage: {}, Data: {}'.format(id, data['ErrorCode'], data['ErrorMessage'], data['Data'])
+    else:
+        print(ret['message'])
--- dataworks-listInstances.py
+++ dataworks-listInstances.py
@@ -3,6 +3,8 @@ import json
 import sys
 from util import App
 from util import unicode_convert
+from util import write_file
+import util
 
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -11,6 +13,7 @@ app = App().getDataworksInstance()
 page = 1
 pageSize = 10
 flag = 1
+ids = []
 while flag == 1:
     ret = app.doAction('ListInstances', {'NodeId': 13582, 'ProjectEnv': 'PROD', 'PageNumber': page, 'PageSize': pageSize, 'ProjectId': 157})
     if ret['code'] == 0:
@@ -19,8 +22,11 @@ while flag == 1:
         if 0 <= data['Data']['TotalCount'] < page * pageSize:
             flag = 0
         for item in data['Data']['Instances']:
+            ids.append(item['InstanceId'])
             print 'InstanceId: {},Status: {}, ErrorMessage: {}'.format(item['InstanceId'], item['Status'], item['ErrorMessage'])
         page = page + 1
     else:
         flag = 0
         print(ret['message'])
+dirPath = app.config.env('app', 'dirpath')
+write_file(dirPath, 'instances.txt', util.implode(',', ids))
