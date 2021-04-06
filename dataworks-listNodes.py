4a2662d1f67612d2bd50b88d5d6f1dbf0390b1f6
jeason <779022431@qq.com>
2021-04-06 22:19:23 +0800

    1

--- dataworks-listNodes.py
+++ dataworks-listNodes.py
@@ -15,23 +15,23 @@ projectEnv = app.config.env('dataworks', 'projectEnv')
 projectIds = util.explode(',', projectIdsStr)
 
 
-def listBusiness(projectId):
-    businessData = []
-    page = 1
-    flag = 1
-    while flag == 1:
-        ret = app.doAction('ListBusiness', {'PageNumber': page, 'PageSize': pageSize, 'ProjectId': projectId})
-        if ret['code'] == 0:
-            data = json.loads(ret['data'])
-            if 0 <= data['Data']['TotalCount'] <= page * pageSize:
-                flag = 0
-            for item in data['Data']['Nodes']:
-                businessData.append(item)
-            page = page + 1
+def listBusiness(projectId1):
+    businessData1 = []
+    page1 = 1
+    flag1 = 1
+    while flag1 == 1:
+        ret1 = app.doAction('ListBusiness', {'PageNumber': page1, 'PageSize': pageSize, 'ProjectId': projectId1})
+        if ret1['code'] == 0:
+            data1 = json.loads(ret1['data'])
+            if 0 <= data1['Data']['TotalCount'] <= page1 * pageSize:
+                flag1 = 0
+            for item1 in data['Data']['Nodes']:
+                businessData1.append(item1)
+            page1 = page1 + 1
         else:
-            flag = 0
-            print(ret['message'])
-    return businessData
+            flag1 = 0
+            print(ret1['message'])
+    return businessData1
 
 
 writeData = []
