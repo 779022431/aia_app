9227c3e096676ff2710ff5804ba4f606626a342c
jeason <779022431@qq.com>
2021-05-10 20:39:04 +0800

    rds

---
+++ rds.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import json
+import sys
+from app import App
+import util
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+app = App().getDataworksInstance()
+dirPath = app.config.env('app', 'dirpath')
+pageSize = int(app.config.env('rds', 'pageSize'))
+writeData = []
+page = 1
+flag = 1
+while flag == 1:
+    ret = app.doAction('DescribeDBInstances', {'PageNumber': page, 'PageSize': pageSize})
+    if ret['code'] == 0:
+        data = json.loads(ret['data'])
+        if 0 <= data['TotalRecordCount'] <= page * pageSize:
+            flag = 0
+        for item in data['Items']['DBInstance']:
+            writeData.append(item)
+        page = page + 1
+    else:
+        flag = 0
+        print(ret['message'])
+util.write_file(dirPath, 'rds.txt', json.dumps(writeData))
