9fd48d13646ade2223f70e9c635c07480251c5d0
jeason <779022431@qq.com>
2021-05-17 15:01:15 +0800

    rds，polardb

--- polardb_slow_sql.py
+++ polardb_slow_sql.py
@@ -14,23 +14,32 @@ writeData = []
 readFile = app.config.env('polardb', 'polardbListFile')
 str2 = util.readFile(dirPath + '/' + readFile)
 ids = json.loads(str2)
-startTime = util.time_date(util.time_unix(), "%Y-%m-%d") + "Z"
-endTime = startTime
+now = util.time_unix()
+slowSqlInterval = int(app.config.env('polardb', 'slowSqlInterval'))
+startTime = util.time_date(now - slowSqlInterval, "%Y-%m-%dT%H:%MZ")
+endTime = util.time_date(now, "%Y-%m-%dT%H:%MZ")
 for id in ids:
-    ret = app.doAction('DescribeSlowLogRecords', {'StartTime': startTime, 'EndTime': endTime, 'DBInstanceId': id, 'PageNumber': 1, 'PageSize': 30})
-    if ret['code'] != 0:
-        continue
-    data = json.loads(ret['data'])
-    if data['PageRecordCount'] <= 0:
-        continue
-    items = data['Items']['SQLSlowLog']
-    count = 1
-    for item in items:
-        if count > 10:
-            continue
-        item['DBInstanceId'] = id
-        writeData.append(json.dumps(item))
-        count = count + 1
+    page = 1
+    flag = 1
+    while flag == 1:
+        ret = app.doAction('DescribeSlowLogRecords', {'StartTime': startTime, 'EndTime': endTime, 'DBInstanceId': id, 'PageNumber': page, 'PageSize': pageSize})
+        print ret
+        exit()
+        if ret['code'] != 0:
+            break
+        data = json.loads(ret['data'])
+        if data['PageRecordCount'] <= 0:
+            break
+        if data['PageRecordCount'] < page * pageSize:
+            flag = 0
+        page = page + 1
+        items = data['Items']['SQLSlowRecord']
+        for item in items:
+            # 超过十秒记录
+            if item['QueryTimes'] < 10:
+                continue
+            item['DBInstanceId'] = id
+            writeData.append(json.dumps(item))
 # 数据写入文件
 fileName = app.config.env('polardb', 'polardbSlowSqlFile')
 util.write_file(dirPath, fileName, util.implode("\n", writeData))
--- rds_slow_sql.py
+++ rds_slow_sql.py
@@ -14,23 +14,31 @@ writeData = []
 readFile = app.config.env('rds', 'rdsListFile')
 str2 = util.readFile(dirPath + '/' + readFile)
 ids = json.loads(str2)
-startTime = util.time_date(util.time_unix(), "%Y-%m-%d") + "Z"
-endTime = startTime
+now = util.time_unix()
+slowSqlInterval = int(app.config.env('rds', 'slowSqlInterval'))
+startTime = util.time_date(now - slowSqlInterval, "%Y-%m-%dT%H:%MZ")
+endTime = util.time_date(now, "%Y-%m-%dT%H:%MZ")
 for id in ids:
-    ret = app.doAction('DescribeSlowLogs', {'StartTime': startTime, 'EndTime': endTime, 'DBInstanceId': id, 'PageNumber': 1, 'PageSize': 30})
-    if ret['code'] != 0:
-        continue
-    data = json.loads(ret['data'])
-    if data['PageRecordCount'] <= 0:
-        continue
-    items = data['Items']['SQLSlowLog']
-    count = 1
-    for item in items:
-        if count > 10:
-            continue
-        item['DBInstanceId'] = id
-        writeData.append(json.dumps(item))
-        count = count + 1
+    page = 1
+    flag = 1
+    while flag == 1:
+        ret = app.doAction('DescribeSlowLogRecords', {'StartTime': startTime, 'EndTime': endTime, 'DBInstanceId': id, 'PageNumber': page, 'PageSize': pageSize})
+        if ret['code'] != 0:
+            break
+        data = json.loads(ret['data'])
+        if data['PageRecordCount'] <= 0:
+            break
+        if data['PageRecordCount'] < page * pageSize:
+            flag = 0
+        page = page + 1
+        items = data['Items']['SQLSlowRecord']
+        for item in items:
+            # 超过十秒记录
+            if item['QueryTimes'] < 10:
+                continue
+            item['DBInstanceId'] = id
+            writeData.append(json.dumps(item))
+
 # 数据写入文件
 fileName = app.config.env('rds', 'rdsSlowSqlFile')
 util.write_file(dirPath, fileName, util.implode("\n", writeData))
