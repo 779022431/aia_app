858136c346c1bf85e024b2c7671d0233efa05454
jeason <779022431@qq.com>
2021-06-03 10:20:57 +0800

    polardb

--- polardb_list.py
+++ polardb_list.py
@@ -11,7 +11,7 @@ app = App().getPolardbInstance()
 dirPath = app.config.env('app', 'dirpath')
 pageSize = int(app.config.env('polardb', 'pageSize'))
 writeData = []
-regions = util.explode(',', app.config.env('polardb', 'regions'))
+regions = util.explode(',', app.config.env('rds', 'regions'))
 for region in regions:
     page = 1
     flag = 1
